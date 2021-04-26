5a8f0983f9347cedecbd49d229d7b6e21cc03b3f
Matt <mlkrambeer20@coe.edu>
2021-04-25 19:05:57 -0500

    Added some more images.  Right now, I am resizing them to perfect square dimensions by hand in paint.  Not sure how to do this in my app automatically.

--- app/src/main/java/edu/coe/krambeer/a15maze/MainActivity.kt
+++ app/src/main/java/edu/coe/krambeer/a15maze/MainActivity.kt
@@ -29,7 +29,7 @@ class MainActivity : AppCompatActivity(), TileViewListener, ImageTileViewListene
 
     private var gameDone = false
 
-    private var picture: Int = R.drawable.dogecoin
+    private var picture: Int = R.drawable.doggarlic
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
